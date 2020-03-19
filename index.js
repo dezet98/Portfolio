1344fdbdf170eef24ec8e9eb0f6e5326426398f4
dezet98 <danielzaczek124@gmail.com>
2020-03-19 19:46:12 +0100

    First commit
    Create header, menu, subpages and link all together to work in Jquery

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        $(".header").addClass("sticky");
+        $(".aboutMe").css("margin-top", $(".header").height());
+
+        if ($(window).scrollTop() == 0) {
+            $(".header").removeClass("sticky");
+            $(".aboutMe").css("margin-top", '0');
+        }
+    });
+
+    // scroll to subpages:
+    $(".menu li").click(function() {
+        var className = '.' + $(this).attr("id");   // id of li(which scroll us to subpage) is this same as class subpage
+        $("html, body").animate({scrollTop: $(className).offset().top - $(".header").height()}, 500, function() {
+            //end of animate, maybe here i remove sth
+        });
+    });
+});
